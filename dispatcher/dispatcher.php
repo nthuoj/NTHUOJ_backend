9f8b81aecdd9e5b3cfd611d730a770b624fce732
侯佳欣 <marquis.js1215@gmail.com>
2015-04-04 23:15:19 +0800

    dispatcher

---
+++ dispatcher/dispatcher.php
@@ -0,0 +1,112 @@
+<?php
+/************************
+dispatcher.php
+This process continuously checks whether there exist unjudged submissions.
+If there's unjudged submissions, start 'submit_curl.php' process to send it to a available judge VM.
+**************************/
+
+session_start();
+
+//require_once("/var/www/lib/database_tools.php");
+require_once("dispatcherFunction.php");
+
+
+writeLog("================================");
+writeLog("================================");
+$cmd = "chmod 666 ../log/dispatcher.log";
+shell_exec($cmd);
+$machine = getMachine();
+if($machine!=null)
+	initMachine($machine);
+else
+	writeLog("get machine file error");
+print_r($machine);
+
+$i=0;
+echo "submit_test start<br>";
+writeLog("dispatcher start");
+while(1)
+{
+	/*sleep(10);*/
+	$con = get_database_object();
+    $sidQuery = "SELECT * FROM problem_submission WHERE status = 'WAIT' ORDER BY id ASC LIMIT 100";
+    $sidRs = mysql_query($sidQuery) or die(mysql_error());
+	
+	echo "sidRsNum".mysql_num_rows($sidRs)."\n";
+	while( $sidRow = mysql_fetch_array($sidRs) ) {
+		$sid = $sidRow['id'];	
+		$pid = $sidRow['problem_id'];
+		$problemQuery = "SELECT * FROM problem_problem where id = '$pid'";
+		$problemRs = mysql_query($problemQuery);
+		$problemRow = mysql_fetch_array($problemRs);
+		$judge_source = $problemRow['judge_source'];
+		$outside_web = $problemRow['judge_type']; 
+		$codeLanType = $sidRow['language'];
+		//getJudgeInfo($pid, $sid);
+		
+		if($judge_source == "LOCAL")
+		{	
+			$findMachine = 0;
+			echo "before findmachine\n";
+			writeLog("before find machine");
+			$destMachine = null;
+			while($destMachine==null)
+			{
+				$destMachine = getIdleMachine($machine);
+				sleep(1);
+			}
+
+			if($destMachine != null)
+			{
+				echo "destMachineName=".$destMachine['machineName']."   destMachineIP=".$destMachine['machineIP']."\n";
+				writeLog("submission ".$sid." use ".$destMachine['machineName'].":".$destMachine['machineIP']."to judge");
+			}
+			else
+			{
+				echo "destMachine=null!!\n";
+				writeLog("find machine error");
+			}
+		
+			$con = get_database_object();
+			$updQuery = "UPDATE problem_submission SET status = 'JUDGING' WHERE id = ".$sid;
+			echo $updQuery."   ".$sid."\n";
+			mysql_query($updQuery) or die(mysql_error());
+			$judgeIP = $destMachine['machineIP'];
+			$judgePage = "interface.php";
+			$judgeURL = $judgeIP."/".$judgePage;
+			echo "judgeURL=".$judgeURL."\n";	
+		
+			echo "send!\n";
+
+			writeLog("send info to judge ".$destMachine['machineName'].":".$destMachine['machineIP']);
+			$arg = $sid." ".$pid." ".$codeLanType." ".$destMachine['machineName']." ".$judgeURL;
+			$handle = popen("/usr/bin/php submit_curl.php ".$arg." & >> output.html", "w");
+			pclose($handle);
+			echo "after popen\n";
+		}
+		else
+		{
+			$con = get_database_object();
+            $updQuery = "UPDATE problem_submission SET status = 'JUDGING' WHERE id = ".$sid;
+            echo $updQuery."   ".$sid."\n";
+            mysql_query($updQuery) or die(mysql_error());
+			
+			writeLog("send info to other judge");
+			$arg = $codeLanType." ".$pid." ".$outside_web." ".$sid;
+			if(file_exists("/var/nthuoj/outsideConnection/sendToOtherJudge.sh"))
+			{
+				$handle = popen("/var/nthuoj/outsideConnection/sendToOtherJudge.sh ".$arg." & >> output.html", "w");
+				pclose($handle);
+			}
+			else
+				writeLog("send info to other judge error(no judge)");
+
+		}
+	}
+
+	sleep(10);
+	$i++;
+}
+echo "submit_test finished\n";
+writeLog("dispatcher finished");
+?>
---
+++ dispatcher/dispatcherFunction.php
@@ -0,0 +1,138 @@
+<?php
+/************************
+dispatcherFunction.php
+This php provides dispatcher related functions.
+Mainly for 'dispatcher.php', 'submit_curl.php' and 'resultUpdater.php'
+************************/
+require_once(dirname(__FILE__)."/lib/database_tools.php");
+
+$machineStatusDir="machineStatus/";
+$logFilePath="../log/dispatcher.log";
+
+/************************
+This function load information from machine.config
+*************************/
+function getMachine()
+{
+	//load machine information from machine.config
+	$handle = fopen("machine.config", "r");
+	if($handle)
+	{
+		$i=0;
+		while ($buffer = fscanf($handle,"%s %s\n"))
+		{
+			list($machineName[$i], $machineIP[$i]) = $buffer;
+			$result[] = array(
+                                'machineName' => $machineName[$i],
+                                'machineIP' => $machineIP[$i]);
+			echo "machinefile:".$i." machineName: ".$machineName[$i]." machineIP: ".$machineIP[$i]."<br>\n";
+			$i++;
+		}
+	}
+	fclose($handle);
+	return $result;
+}
+
+/**********************
+This initials files act as the communication between dispatccher and resultUpdater.php.
+Each file indicates that whether a machine (judge VM) is available or not.
+***********************/
+function initMachine($machine)
+{
+	global $machineStatusDir;
+	$cmd = "test -d " . $machineStatusDir . " && rm -r " . $machineStatusDir . "; mkdir " . $machineStatusDir;
+	pclose(popen($cmd, "r"));
+	foreach($machine as $value)
+	{
+		$cmd = "echo 0 > ".$machineStatusDir.$value['machineName'].".status";
+		pclose(popen($cmd, "r"));
+	}
+}
+
+/***********************
+Given '$machine' with all machines' information, return a machine who is available to judge right now.
+************************/
+function getIdleMachine($machine)
+{
+	global $machineStatusDir;
+	$destMachine=null;
+	foreach($machine as $value)
+	{
+		$handle = fopen($machineStatusDir.$value['machineName'].".status", "r");
+		if($handle)
+    	{
+			$buffer = fscanf($handle,"%d");
+			list($status) = $buffer;
+		}
+		fclose($handle);
+		if($status==0)
+		{
+			echo "find Idle Machine".$value['machineName']."\n";
+			setMachineStatus($value['machineName'], 1);
+			//$cmd = "echo 1 > ".$machineStatusDir.$value['machineName'].".status";
+			//pclose(popen($cmd, "r"));
+			$destMachine['machineName'] = $value['machineName'];
+			$destMachine['machineIP'] = $value['machineIP'];
+			break;
+		}
+	}
+	return $destMachine;
+}
+
+/**************************
+Given '$machineName' and '$status', set the '$status' into the specific machine's status file.
+**************************/
+function setMachineStatus($machineName, $status)
+{
+	global $machineStatusDir;
+	$cmd = "echo ".$status." > ".$machineStatusDir.$machineName.".status";
+	//pclose(popen($cmd, "r"));
+	shell_exec($cmd);
+}
+
+/*******************************
+Given '$pid' and '$sid', get the required information to judge a submission: tid, time/memory limit, special judge...
+********************************/
+function getSubmissionInfo($pid, $sid)
+{
+	$con = get_database_object();
+	$tidQuery = "SELECT * FROM problem_testcase WHERE problem_id = '$pid'";
+        $tidRs = mysql_query($tidQuery) or die(mysql_error());
+        $j=0;
+        while( $tidRow = mysql_fetch_array($tidRs))
+        {
+			$tid[]=$tidRow['id'];
+			$timeLimit[] = $tidRow['time_limit'];
+			$memoryLimit[] = $tidRow['memory_limit'];
+			echo "tid:".$tid[$j]."\n";
+			echo "timeLimit:".$timeLimit[$j]."\n";
+			echo "memoryLimit:".$memoryLimit[$j]."\n";
+			$j++;
+        }
+
+        $pidQuery = "SELECT * FROM problem_problem WHERE id = '$pid'";
+        $pidRs = mysql_query($pidQuery) or die(mysql_error());
+        $pidRow = mysql_fetch_array($pidRs);
+        $judge_type = $pidRow['judge_type'];
+        $judge_language = $pidRow['judge_language'];
+	echo "judge_type ".$judge_type."\n";
+	echo "judge_language ".$judge_language."\n"; 
+
+	$result = array( "tid"=>$tid,
+                         "timeLimit"=>$timeLimit, "memoryLimit"=>$memoryLimit,
+                         "judge_type"=>$judge_type, "judge_language"=>$judge_language);
+	return $result;
+}
+
+/************************
+This write '$msg' to dispatcher's log file.
+************************/
+function writeLog($msg)
+{
+	global $logFilePath;
+	$time = "[".date("D M j G:i:s Y")."]  ";
+	$cmd = "echo ".$time.$msg." >> ".$logFilePath;
+    	//pclose(popen($cmd, "r"));
+	shell_exec($cmd);
+}
+?>

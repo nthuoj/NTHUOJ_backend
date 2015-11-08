9a1b12c5c8b6bb89f95f6db5afe17be321018cd2
shawn1128 <demonboy2266@gmail.com>
2015-11-09 02:04:14 +0800

    Update dispatcher.py
    modify dispatcher.py. Not done

--- ojbackend/dispatcher.py
+++ ojbackend/dispatcher.py
@@ -7,7 +7,10 @@ from dispatcherFunc import *
 import time
 import logging
 import subprocess, sys
-logging.basicConfig(filename = 'dispatcher.log', level = logging.INFO, format = '%(asctime)s ::%(message)s',datafmt = '%m/%d/%Y %I:%M:%S %p')
+logging.basicConfig(filename = 'dispatcher.log', 
+                    level = logging.INFO, 
+                    format = '%(asctime)s ::%(message)s',
+                    datafmt = '%m/%d/%Y %I:%M:%S %p')
 logging.info('==========Dispatcher Started==========')
 
 machineInfo = getMachine()
@@ -36,9 +39,15 @@ while True:
         logging.info('Please check the settings in ojdatabase.config')
         logging.info('==========Dispatcher Finished==========')
         exit(0)
+    submissionStat = " \
+    SELECT * FROM problem_submission \
+    WHERE status = \'WAIT\' \
+    ORDER BY id ASC \
+    LIMIT 100; \
+    "
     cur = DB.cursor()
     cur.execute("USE nthuoj;")
-    cur.execute("SELECT * FROM problem_submission WHERE status = 'WAIT' ORDER BY id ASC LIMIT 100;")
+    cur.execute(submissionStat)
     sidQuery = cur.fetchone()
     sid = 1
     pid = 2
@@ -49,30 +58,49 @@ while True:
         pid = sidQuery[1]
         logging.info('sidQuery Success!')
         logging.info('sid = %d, pid = %d' % (sid, pid))
-        cur.execute("SELECT * FROM problem_problem where id = '%d';" % pid)
+        problemID = " \
+        SELECT * FROM problem_problem where id = \'%d\'; \
+        "
+        cur.execute(problemID % pid)
         pidQuery = cur.fetchone()
         judgeSource = pidQuery[11]
         judgeType = pidQuery[12]
         judgeLanguage = pidQuery[13]
         logging.info('pidQuery Success!')
-        logging.info('judgeSource = %s, judgeType = %s, judgeLanguage = %s' % (judgeSource, judgeType, judgeLanguage))
+        logging.info('judgeSource = %s, judgeType = %s, judgeLanguage = %s' 
+                    % (judgeSource, judgeType, judgeLanguage))
 
         if judgeSource == "LOCAL":
             idleMachine = None
             logging.info('get idleMachine')
-            while(idleMachine == None):
-                idleMachine = getIdleMachine()
+            while idleMachine == None:
                 time.sleep(1)
-            logging.info('idleMachine = %s' % idleMachine)
-            cur.execute("UPDATE problem_submission SET status = 'JUDGING' WHERE id = '%d';" % sid)
+                idleMachine = getIdleMachine()       
+            logging.info('idleMachine = %s' % idleMachine) 
+            updatesidStat = " \
+            UPDATE problem_submission \
+            SET status = \'JUDGING\' \
+            WHERE id = \'%d\'; \
+            "
+            cur.execute(updatesidStat % sid)
             DB.commit()
             judgeIP = machineInfo[idleMachine]
             judgeURL = judgeIP + "/interface.py"
             logging.info('get judgeURL = %s' % judgeURL)            
         else :
-            cur.execute("UPDATE problem_submission SET status = 'JUDGING' WHERE id = '%d';" % sid)
+            updatesidStat = " \
+            UPDATE problem_submission \
+            SET status = \'JUDGING\' \
+            WHERE id = \'%d\'; \
+            "
+            cur.execute(updatesidStat % sid)
             logging.info('send info to other judge')
-            """
-		to be continued
-            """
+            arg = judgeLanguage + " " + pid + " " + judgeURL + " " + sid
+
+            if os.path.exists("/var/nthuoj/outsideConnection/sendToOtherJudge.sh"):
+                handle = popen("/var/nthuoj/outsideConnection/sendToOtherJudge.sh " + 
+                               arg + " & >> output.html", "w")
+                pclose(handle)
+            else :
+                logging.info('Send to other judge ERROR(No Judge)')
     time.sleep(1)
